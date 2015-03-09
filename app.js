50b119693758f88ac9a7b1f8d56e0212085c5471
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-03-09 16:49:50 -0400

    Create addComment() function for PostsCtrl

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.controller('MainCtrl', [
             $scope.posts.push({
                 title: $scope.title, 
                 link: $scope.link,
-                upvotes: 0
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
             $scope.title = '';
             $scope.link = '';
@@ -30,6 +34,25 @@ app.controller('MainCtrl', [
         
         $scope.incrementUpvotes = function(post) {
             post.upvotes += 1;
+        };
+        
+    }]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+        
+        $scope.addComment = function() {
+            if($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
         };
         
     }]);
@@ -44,7 +67,12 @@ app.config([
                 url: '/home',
                 templateUrl: '/home.hmtl',
                 controller: 'MainCtrl'
-        });
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
         
         $urlRouterProvider.otherwise('home');
     }]);
